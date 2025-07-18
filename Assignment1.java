21b2e6487d0b32dd204335c642263bf790a2fdda
danle524 <nkdanle524@gmail.com>
2025-07-18 13:39:20 -0700

    initial commit

---
+++ Assignment1.java
@@ -0,0 +1,8 @@
+public class Assignment1 {
+    public static void main(String[] args){
+
+        System.out.println("CSC142-Intro to Java");
+        System.out.println("One thing I learned from module 1 is I know that ");
+        System.out.print("we can still use Visual Studio Code which is simpler than Eclipse for coding");
+    }
+}
---
+++ Codestepbystep.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+public class Codestepbystep {
+    public static void main(String[] args) {
+        String[] fruit = {"apple", "mango"};
+        System.out.println(fruit);
+        System.out.println(Arrays.toString(fruit));
+    }
+}
